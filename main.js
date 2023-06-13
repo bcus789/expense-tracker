cce3807677b6a44ae8ee6925fc04674a57116d71
Bonello <Geno.Bonello@treasurer.ca.gov>
2023-06-13 10:41:40 -0700

    added JS

--- main.js
+++ main.js
@@ -1,27 +1,84 @@
-const inputEl = document.getElementById("input-el")
-const submitBtn = document.getElementById("submit-btn")
-const listEl = document.getElementById("list")
-const removeBtn = document.getElementById("removeBtn")
-let list = []
+let expenses = [];
+let totalAmount = 0;
 
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const addBtn = document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expnese-table-body');
+const totalAmountCell = document.getElementById('total-amount');
 
+addBtn.addEventListener('click', function() {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
 
-submitBtn.addEventListener("click", function(){
-    list.push(inputEl.value)
-    inputEl.value = ""
-    renderList()
-})
-
-function renderList(){
-    let listItems = ""
-    for (let i = 0; i<list.length; i++){
-        listItems += `<p class="listitems" id="${[i]}">${list[i]}<button id="removeBtn" onclick="remove(${[i]})">X</button></p>`
+    if (category === '') {
+        alert('Please select a category');
+        return;
     }
-    listEl.innerHTML = listItems
-}
+    if (isNaN(amount) || amount <=0 ) {
+        alert('Please enter a valid amoun')
+        return;
+    }
+    if(date === '') {
+        alert('Please select a date')
+        return;
+    }
+    expenses.push({category, amount, date});
 
-function remove(item){
-    list.splice(item, 1)
-    console.log(list)
-    renderList()
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+    });
+
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+
+});
+
+for (const expense of expenses) {
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.inserRow();
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+    });
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
 }
