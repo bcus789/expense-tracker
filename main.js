7cfa4494e26c67f377e38c87d517ad76e5c3e550
Bonello <Geno.Bonello@treasurer.ca.gov>
2023-06-13 14:56:28 -0700

    got list population working by targeting objects in array

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const categorySelect = document.getElementById('category-select');
 const amountInput = document.getElementById('amount-input');
 const dateInput = document.getElementById('date-input');
 const addBtn = document.getElementById('add-btn');
-const expensesTableBody = document.getElementById('expnese-table-body');
+const expensesTableBody = document.getElementById('expenses-table-body');
 const totalAmountCell = document.getElementById('total-amount');
 
 addBtn.addEventListener('click', function() {
@@ -13,16 +13,29 @@ addBtn.addEventListener('click', function() {
     const amount = Number(amountInput.value);
     const date = dateInput.value;
 
-    if (category === '') {
-        alert('Please select a category');
-        return;
-    }
-    if (isNaN(amount) || amount <=0 ) {
-        alert('Please enter a valid amoun')
-        return;
-    }
-    if(date === '') {
-        alert('Please select a date')
-        return;
-    }
+    // if (category === '') {
+    //     alert('Please select a category');
+    //     return;
+    // }
+    // if (isNaN(amount) || amount <=0 ) {
+    //     alert('Please enter a valid amoun')
+    //     return;
+    // }
+    // if(date === '') {
+    //     alert('Please select a date')
+    //     return;
+    // } else {
+        
+    // }
+
+    expenses.push({category, amount, date})
+    render(expenses)
 })
+
+function render(expense){
+    let expenseItems = ""
+    for (let i=0; i<expense.length; i++){
+        expenseItems += `<tr><td>${expense[i].category}</td><td>${expense[i].amount}</td><td>${expense[i].date}</td><td><button id="delete-btn">Delete</button></td><tr>`
+    }
+    expensesTableBody.innerHTML = expenseItems
+}
